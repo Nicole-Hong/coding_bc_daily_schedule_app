540ea9afc12681eec7c32280b4532ffe6a50b1bf
Nicole-Hong <n.hong@queensu.ca>
2023-11-05 16:32:13 -0800

    upload the content

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
-# Work Day Scheduler Starter Code
+# Module 5 - Work Day Scheduler Starter Code
 
+## About This Challenge
+
+The objective of this module challenge is to create an interactive program that allows users to record and save their events in an hour-interval on a daily schedule. The program involves JavaScript that utilizes the Document Object Model (DOM), the data storage into and retrieval from Local Storage and Bootstrap. jQuery and JS libraries were utilized to capture the datetime activities.
+
+
+## Built With
+
+* HTML
+* CSS
+* Javascript (jQuery & Bootstrap)
+
+
+## Website Deployed
+
+* Random Password Generator URL: https://nicole-hong.github.io/coding_bc_webAPI_CodeQuiz/ 
+
+
+## Application Demo - Screenshots
+
+1. Open the Daily Schedule URL provided above.
+2. Layout of the User Interface
+![UI_schedule]
+
+![event]
+
+![change]
+
+![new]
 
 
 ## Reference Link:
 * Starter Code: https://github.com/coding-boot-camp/crispy-octo-meme 
+* Javascript Library: https://cdnjs.com/libraries 
--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+//Display Current Date in Header
+var currentDay = $("#currentDay");
+currentDay.text (moment().format('dddd, MMM Do YYYY'));
+
+//Find Current Hour
+var currentHour = moment().hour();
+
+
+//$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +30,33 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+// });
+
+
+//Format Text Areas
+$(".time-block").each(function(){
+  //Get Hour Value of Each Time Block
+  var blockHour = $(this).attr("id").split("-")[1];
+  //Get Saved Value from Local Storage and Display in Correct Time Block's Text Area
+  var textEntry = localStorage.getItem(blockHour);
+  var textArea = $(this).find(".description");
+  textArea.val (textEntry);
+  //Compare Current Hour to Time Block, Add Appropriate Class to Display Correct Color
+  if (blockHour < currentHour){
+      $(this).find(".description").addClass("past");
+  }else if(blockHour == currentHour){
+      $(this).find(".description").addClass("present");
+  }else{
+      $(this).find(".description").addClass("future");
+   }
+});
+
+//Save Button Clicked Function
+$(".saveBtn").on("click", function(){
+  //Key
+  var key = $(this).parent().attr("id").split("-")[1];
+  //Value
+  var value = $(this).parent().find(".description").val();
+  //Save Key and Value to Local Storage
+  localStorage.setItem(key,value);
 });
